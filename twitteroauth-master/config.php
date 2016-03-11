e3c56795d5084b1f75d1c95fa83a00d09af0c91f
Christian <christian130@gmail.com>
2016-03-11 18:54:07 -0430

    equis

---
+++ twitteroauth-master/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'sHQDVwHE6N0A45QlR5Q');
+define('CONSUMER_SECRET', 'IxG87DcyIYiQC3QyYEXt5zmhTvm2YbRp8XGF1ZwSIs');
+define('OAUTH_CALLBACK', 'http://whatsdadilly.com/beta/twitteroauth-master/callback.php');
